be3d8f66ad7abf76fc8a84e6dcdd5cb7df174ecb
hachimitsue <jusalyng@gmail.com>
2024-12-20 21:51:20 +0800

    feat: enhance UserTable component with avatar integration and improved card styling
    Co-Authored-By: Chilitooo <160409269+Chilitooo@users.noreply.github.com>
    Co-Authored-By: melvinjipos <160334174+melvinjipos@users.noreply.github.com>
    Co-Authored-By: michelleborromeo <160455420+michelleborromeo@users.noreply.github.com>
    Co-Authored-By: reonestarjayleespina <122802235+reonestarjayleespina@users.noreply.github.com>

--- frontendV2/src/views/dashboard/UserTable.vue
+++ frontendV2/src/views/dashboard/UserTable.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { defineProps } from "vue";
-
+import avatarImage from "@/assets/images/user1.png";
 
 const props = defineProps({
   userData: {
@@ -35,18 +35,18 @@ const resolveUserStatusVariant = (stat) => {
 </script>
 
 <template>
-  <v-card>
+  <v-card class="bg" elevation="10">
     <v-data-table
       :headers="headers"
       :items="userData"
       item-value="id"
-      class="text-no-wrap"
+      class="text-no-wrap bg"
     >
       <!-- User -->
       <template #item.username="{ item }">
         <div class="d-flex align-center" style="gap: 15px">
-          <v-avatar size="34" :variant="!item.avatar ? 'tonal' : undefined">
-            <v-img v-if="item.avatar" :src="item.avatar" />
+          <v-avatar size="34">
+            <v-img :src="avatarImage" />
           </v-avatar>
 
           <div class="d-flex flex-column">
@@ -71,3 +71,15 @@ const resolveUserStatusVariant = (stat) => {
     </v-data-table>
   </v-card>
 </template>
+
+<style scoped>
+.bg {
+  /* From https://css.glass */
+  background: rgba(77, 182, 172, 0.053);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(77, 182, 172, 0.3);
+}
+</style>
